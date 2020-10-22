0dfb4e089074fe5b6d10693e1ddd56ced384c81d
Costa <matheus.costa.ol97@gmail.com>
2020-10-22 12:33:15 -0300

    successo compra e index

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -1,130 +1,129 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
 export default props => (
-<html lang="pt-br">
 
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
-  <link rel="stylesheet" href="../css/main.css"/>
-  <link rel="stylesheet" href="../css/index.css"/>
-  <title>Home</title>
-</head>
+<>
 
-<body>
-
-  <div className="container">
-    <div className="row"></div>
+<div class="container">
+    <div class="row"></div>
   </div>
-  <div className="container">
-    <div className="row">
+  <div class="container">
+    <div class="row">
 
-      <div className="col-12">
+      <div class="col-12">
 
-      </div>
-
-      <div className="col-12">
+      </div>      <div class="col-12">
         <section>
-
-          <div className="center carousel">
-            <div className="banners-carousel">
-
+                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100 banners-carousel" src="../img/champagne-dom-perignon-brut_1_650.jpg" alt="First slide"/>
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100 banners-carousel" src="../img/desvinho_logo.png" alt="Second slide"/>
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100 banners-carousel" src="../img/cart_icon.png" alt="Third slide"/>
+              </div>
             </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
           </div>
-   
-          <div className="products">
+          <div class="products">
 
-            <div className="header-products">
+            <div class="header-products">
               <h2>Bebidas</h2>
             </div>
 
-            <div className="container">
-              <div className="row">
+            <div class="container">
+              <div class="row">
 
-                <div className="col-6 col-md-3">
-                  <div className="center product">
-                    <div className="image-product">
-                      <img
-                        src="../img/Chivas-12-anos (3).png"              alt="Chivas 12 anos"/>
+                <div class="col-6 col-md-3">
+                  <div class="center product">
+                    <div class="image-product">
+                      <img src="../img/Chivas-12-anos (3).png" alt="Chivas 12 anos"/>
                     </div>
-                    <div className="header-product">
-                      <h1 className="header-product">Chivas 12 anos</h1>
+                    <div class="header-product">
+                      <h1 class="header-product">Chivas 12 anos</h1>
                     </div>
-                    <div className="price-product"><label className="price-line">R$ 99,99</label> <label>R$ 90,99</label></div>
+                    <div class="price-product"><label class="price-line">R$ 99,99</label> <label>R$ 90,99</label></div>
                     <div>
-                      <button className="buy-product">comprar</button>
+                      <button class="buy-product">comprar</button>
                     </div>
                   </div>
                 </div>
 
-                <div className="col-6 col-md-3">
-                  <div className="center product">
+                <div class="col-6 col-md-3">
+                  <div class="center product">
 
-                    <div className="image-product">
-                      <img
-                        src="../img/Chivas-12-anos (3).png"
-                        alt="Chivas 12 anos"/>
+                    <div class="image-product">
+                      <img src="../img/Chivas-12-anos (3).png" alt="Chivas 12 anos"/>
                     </div>
-                    <div className="header-product">
-                      <h1 className="header-product">Chivas 12 anos</h1>
+                    <div class="header-product">
+                      <h1 class="header-product">Chivas 12 anos</h1>
                     </div>
-                    <div className="price-product"><label className="price-line">R$ 99,99</label> <label>R$ 90,99</label></div>
+                    <div class="price-product"><label class="price-line">R$ 99,99</label> <label>R$ 90,99</label></div>
                     <div>
-                      <button className="buy-product">comprar</button>
+                      <button class="buy-product">comprar</button>
                     </div>
-                    
+
                   </div>
                 </div>
 
-                <div className="col-6 col-md-3">
-                  <div className="center product">
-                    <div className="image-product">
-                      <img
-                        src="../img/Chivas-12-anos (3).png"
-                        alt="Chivas 12 anos"/>
+                <div class="col-6 col-md-3">
+                  <div class="center product">
+                    <div class="image-product">
+                      <img src="../img/Chivas-12-anos (3).png" alt="Chivas 12 anos"/>
                     </div>
-                    <div className="header-product">
-                      <h1 className="header-product">Chivas 12 anos</h1>
+                    <div class="header-product">
+                      <h1 class="header-product">Chivas 12 anos</h1>
                     </div>
-                    <div className="price-product"><label className="price-line">R$ 99,99</label> <label>R$ 90,99</label></div>
+                    <div class="price-product"><label class="price-line">R$ 99,99</label> <label>R$ 90,99</label></div>
                     <div>
-                      <button className="buy-product">comprar</button>
+                      <button class="buy-product">comprar</button>
                     </div>
                   </div>
                 </div>
 
-                <div className="col-6 col-md-3">
-                  <div className="center product">
-                    <div className="image-product">
-                      <img
-                        src="../img/Chivas-12-anos (3).png"
-                        alt="Chivas 12 anos"/>
+                <div class="col-6 col-md-3">
+                  <div class="center product">
+                    <div class="image-product">
+                      <img src="../img/Chivas-12-anos (3).png" alt="Chivas 12 anos"/>
                     </div>
-                    <div className="header-product">
-                      <h1 className="header-product">Chivas 12 anos</h1>
+                    <div class="header-product">
+                      <h1 class="header-product">Chivas 12 anos</h1>
                     </div>
-                    <div className="price-product"><label className="price-line">R$ 99,99</label> <label>R$ 90,99</label></div>
+                    <div class="price-product"><label class="price-line">R$ 99,99</label> <label>R$ 90,99</label></div>
                     <div>
-                      <button className="buy-product">comprar</button>
+                      <button class="buy-product">comprar</button>
                     </div>
                   </div>
                 </div>
 
-                <div className="col-6 col-md-3">
-                  <div className="center product">
-                    <div className="image-product">
-                      <img
-                        src="../img/Chivas-12-anos (3).png"
-                        alt="Chivas 12 anos"/>
+                <div class="col-6 col-md-3">
+                  <div class="center product">
+                    <div class="image-product">
+                      <img src="../img/Chivas-12-anos (3).png" alt="Chivas 12 anos"/>
                     </div>
-                    <div className="header-product">
-                      <h1 className="header-product">Chivas 12 anos</h1>
+                    <div class="header-product">
+                      <h1 class="header-product">Chivas 12 anos</h1>
                     </div>
-                    <div className="price-product"><label className="price-line">R$ 99,99</label> <label>R$ 90,99</label></div>
+                    <div class="price-product"><label class="price-line">R$ 99,99</label> <label>R$ 90,99</label></div>
                     <div>
-                      <button className="buy-product">comprar</button>
+                      <button class="buy-product">comprar</button>
                     </div>
                   </div>
                 </div>
@@ -140,7 +139,6 @@ export default props => (
   </div>
 
 
-</body>
+</>
 
-</html>
 )
