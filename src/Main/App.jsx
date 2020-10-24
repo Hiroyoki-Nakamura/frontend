37dbc5913f5f712da23b9db5d44ebad79b930113
Kleydson Barros Vieira <Kleydson122@gmail.com>
2020-10-24 19:04:16 -0300

    home page quase completa

--- src/Main/App.jsx
+++ src/Main/App.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Routes from './Routes';
 import Footer from '../templates/Footer';
 import Header from '../templates/Header';
 import Menu from '../templates/NavBar';
 
-export default props => (
-  <>
-    <Header />
-    <Menu />
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <Routes />
-        </div>
-      </div>
-    </div>
+export default class App extends Component {
 
-    <Footer />
-  </>
-);
+
+  render() {
+    return (
+      <>
+        <Header />
+        <Menu />
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <Routes />
+            </div>
+          </div>
+        </div>
+
+        <Footer />
+      </>
+    );
+  }
+}
