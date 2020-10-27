a7d86cb82eef998b5fc746957508e9879c90687c
Gustavo Doria <gustavogomesdoria@outlook.com>
2020-10-27 00:19:48 -0300

    update project

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import App from './Main/App';
+ReactDOM.render(
+  <App /> ,
+document.getElementById('root')
+);
