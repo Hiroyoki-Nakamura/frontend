ed08f8b650be1cdf68979344e2b8f8a69abffcd7
Costa <matheus.costa.ol97@gmail.com>
2020-10-26 21:58:27 -0300

    layout categoria ajustado

--- src/pages/Categoria/index.jsx
+++ src/pages/Categoria/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles.css';
 
 export default props => (
 <html lang="pt-br">
@@ -6,12 +7,6 @@ export default props => (
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
-  <link rel="stylesheet" href="../css/main.css" />
-  <link rel="stylesheet" href="../css/index.css" />
-  <link rel="stylesheet" href="../css/categorias.css" />
-  <link rel="icon" href="../img/glasswine_icon.png" />
   <title>Categorias</title>
 </head>
 
