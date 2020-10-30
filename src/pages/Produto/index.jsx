407ee084c1d2c169c20712e9da9019f1b04d97f3
Steffany Sousa <steffanygabriella@gmail.com>
2020-10-30 12:08:38 -0300

    alteracaoexpan

--- src/pages/Produto/index.jsx
+++ src/pages/Produto/index.jsx
@@ -44,18 +44,16 @@ export default class Produto extends Component {
         <section id="sectionproduto" className="center">
           <div className="container">
             <div className="row">
-              <div className="col-3">
+              <div className="col-lg-3 ">
                 <div className="card style_card">
                   <div className="card-body center">
                     <img className='imageProduct' src={this.state.product.ds_imagem} />
                   </div>
                 </div>
               </div>
-              <article className='col-9'>
-                <div className="col-lg-3">
+              <div className="col-9 md-12">
 
-                </div>
-                <div className="col-12 ">
+                <div className="col-12">
                   <h4>{this.state.product.nome_produto}</h4>
                   <hr />
                   <h6>De: <strike>R$ {`${this.state.product.valor_produto}`.replace('.', ',')}</strike> &nbsp;
@@ -65,14 +63,14 @@ export default class Produto extends Component {
 
                   <p>{this.state.product.ds_produto}</p>
                   <hr />
-                  <div className="col-6" id="comprar">
-                    <a href="#/carrinho"><button type="button" className="btn btn-success btn-lg btn-block" onClick={this.onCart} id="botao">Adicionar ao Carrinho</button></a>
+                  <div className="col-lg-6" id="comprar">
+                    <a href="#/carrinho"><button type="button" className="btn btn-success btn-lg " onClick={this.onCart} id="botao">Comprar</button></a>
                   </div>
-                  <div className="col-4" id="voltar">
-                    <a href="#/"><button type="button" className="btn btn-secondary" id="botao">Voltar</button></a>
+                  <div className="col-6" id="voltar">
+                    <a href="#/"><button type="button" className="btn btn-secondary btn-lg" id="botao">Voltar</button></a>
                   </div>
                 </div>
-              </article>
+              </div>
             </div>
           </div>
         </section>
