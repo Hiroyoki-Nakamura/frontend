6180d9b7fcd01719febb0ce896b61ed1498df2ca
Kleydson Barros Vieira <Kleydson122@gmail.com>
2020-11-10 17:47:28 -0300

    front checkout quase finalizado

--- src/pages/Checkout/index.jsx
+++ src/pages/Checkout/index.jsx
@@ -3,14 +3,21 @@ import './styles.css';
 
 import API from '../../Services/api';
 
-// import Endereco from '../Endereco'
-
 const BEFORE = {
+  ufs: [],
+  pageAdress: 'novoEndereco',
   price: '',
   showHideForm: false,
   client: '',
   page: 'cartao',
   enderecos: [],
+  rua: '',
+  bairro: '',
+  complemento: '',
+  referencia: '',
+  numero: '',
+  cep: '',
+  uf: '',
   nome_titular: '',
   cpf_titular: '',
   numero_cartao: '',
@@ -19,19 +26,15 @@ const BEFORE = {
 }
 
 export default class Checkout extends Component {
-
   state = { ...BEFORE };
 
   componentDidMount() {
-    this.getEndereco();
+    this.initialization();
   }
 
-  showPage = () => {
-
-  }
-
-  getEndereco = async () => {
+  initialization = async () => {
     const client = JSON.parse(localStorage.getItem('client'));
+    const ufs = await API.get('/uf');
     const enderecos = await API.get(`/endereco/buscar/${client.id}`);
     const cartSettings = await JSON.parse(localStorage.getItem('cartSettings'));
 
@@ -40,8 +43,45 @@ export default class Checkout extends Component {
     this.setState({
       enderecos: [...enderecos.data],
       price: price,
+      address: enderecos.data[0].id,
+      ufs: [...ufs.data]
+    });
+  } 
+
+  getEndereco = async () => {
+    const client = JSON.parse(localStorage.getItem('client'));
+    const enderecos = await API.get(`/endereco/buscar/${client.id}`);
+    this.setState({
+      enderecos: [...enderecos.data],
       address: enderecos.data[0].id
     });
+  }
+
+  getUfs = async () => {
+    const ufs = await API.get('/uf');
+    this.setState({ ufs: [...ufs.data] });
+  }
+
+  postEnderecos = async () => {
+    const client = JSON.parse(localStorage.getItem('client'))
+    const sendObject = await API.post('/endereco/salvar', {
+      rua: this.state.rua,
+      bairro: this.state.bairro,
+      complemento: this.state.complemento,
+      referencia: this.state.referencia,
+      numero: this.state.numero,
+      cep: this.state.cep,
+      cd_uf: this.state.uf,
+      cd_cliente: client.id
+    });
+
+    if (sendObject.status == 201) {
+      alert(sendObject.data);
+      this.setState({ pageAdress: 'novoEndereco' });
+      this.getEndereco();
+    } else {
+      alert(sendObject.data);
+    }
   }
 
   postCards = async () => {
@@ -70,18 +110,22 @@ export default class Checkout extends Component {
       tipo_pagamento: (this.state.page == 'boleto' ? 1 : 2),
       dados_pagamento,
       produtos: [...cart],
+      frete: '35',
       valor_total: parseFloat(this.state.price.replace(',', '.'))
     };
 
     const sendOrder = await API.post('/pedido/criar', objSend);
 
-    console.log(sendOrder);
+    if (sendOrder.status == 201) {
+      alert('pedido criado com sucesso!');
 
-    // alert(sendOrder );
-    
-    // localStorage.removeItem('cart');
-    // localStorage.removeItem('cartSettings');
-    // window.location.href = '/';
+      // localStorage.removeItem('cart');
+      // localStorage.removeItem('cartSettings');
+      // window.location.href = '#/pedido';
+    } else {
+      alert(sendOrder.data);
+    }
+
   };
 
   onChange = (event) => {
@@ -112,10 +156,121 @@ export default class Checkout extends Component {
       case 'endereco_entrega':
         this.setState({ address: value });
         break;
+      case 'rua':
+        this.setState({ rua: value })
+        break;
+      case 'numero':
+        this.setState({ numero: value })
+        break;
+      case 'bairro':
+        this.setState({ bairro: value })
+        break;
+      case 'complemento':
+        this.setState({ complemento: value })
+        break;
+      case 'referencia':
+        this.setState({ referencia: value })
+        break;
+      case 'cep':
+        this.setState({ cep: value })
+        break;
+      case 'uf':
+        this.setState({ uf: value })
+        break;
       default:
         break;
     }
   };
+
+  renderAdress = event => {
+    this.setState({ pageAdress: event.target.id });
+  }
+
+  showAdress = () => {
+    const pageAdress = this.state.pageAdress;
+    const Adress =
+      <>
+        <h3 className='w-100 text-center'>Endereço de Entrega</h3>
+        <div className='d-flex justify-content-center align-items-center h-75'>
+          <div className="h-auto">
+            <label className='w-100 text-center'> Endereço cadastrado: </label>
+            <select className="custom-select radius" onChange={this.onChange} id="endereco_entrega" >
+              {this.state.enderecos.map(address => {
+                const addressComplete = `${address.cep} - ${address.rua},${address.numero} - ${address.bairro} - ${address.cd_uf}`;
+                return <option key={address.id} value={address.id}>{addressComplete}</option>
+              })}
+
+            </select>
+            <div className='center'>
+
+              <a className="btn btn-primary btn-lg active mt-5 radius" aria-pressed="true" id="endereco" value="endereco" onClick={this.renderAdress} >Entregar em outro Endereço</a>
+            </div>
+          </div>
+        </div>
+      </>
+
+    const newAdress =
+      <>
+        <h3 className='w-100 text-center'>Endereço de Entrega</h3>
+
+        <div className='w-100 height-new-address overflow-auto'>
+
+          <div className="form-group">
+            <label className='w-100 text-center'>Rua</label>
+            <input type="text" className="form-control text-center" id="rua" placeholder="Rua" onChange={this.onChange} value={this.state.rua} />
+          </div>
+
+          <div className="form-group">
+            <label className='w-100 text-center'>Número</label>
+            <input type="text" className="form-control text-center" id="numero" placeholder="Nº" onChange={this.onChange} value={this.state.numero} />
+          </div>
+
+          <div className="form-group">
+            <label className='w-100 text-center'>Bairro</label>
+            <input type="text" className="form-control radius text-center" id="bairro" placeholder="Bairro" onChange={this.onChange} value={this.state.bairro} />
+          </div>
+
+          <div className="form-group">
+            <label className='w-100 text-center'>Complemento</label>
+            <input type="text" className="form-control text-center" id="complemento" placeholder="Complemento" onChange={this.onChange} value={this.state.complemento} />
+          </div>
+
+          <div className="form-group">
+            <label className='w-100 text-center'>Referência</label>
+            <input type="text" className="form-control text-center" id="referencia" placeholder="Referência" onChange={this.onChange} value={this.state.referencia} />
+          </div>
+
+          <div className="form-group">
+            <label className='w-100 text-center'>UF</label>
+            <select className="custom-select form-control text-center radius" id="uf" onChange={this.onChange}>
+              {this.state.ufs.map(uf => {
+                return <option key={uf.id} value={uf.id}>{uf.ds_uf}</option>
+              })}
+            </select>
+          </div>
+
+          <div className="form-group">
+            <label className='w-100 text-center'>CEP</label>
+            <input type="CEP" className="form-control text-center" id="cep" placeholder="00000-000" onChange={this.onChange} value={this.state.cep} />
+          </div>
+
+          <div className="center">
+            <a className="btn btn-primary radius mx-2" id="novoEndereco" aria-pressed="true" onClick={this.renderAdress} value="novoEndereco">Voltar</a>
+            <a className="btn  btn-success mx-2 radius " id="salvar" aria-pressed="true" onClick={this.postEnderecos} >Salvar</a>
+          </div>
+
+        </div>
+      </>
+
+    switch (pageAdress) {
+      case 'endereco':
+        return newAdress;
+      case 'novoEndereco':
+        return Adress;
+      default:
+
+    }
+  }
 
   renderPay = event => {
     this.setState({ page: event.target.value });
@@ -125,19 +280,18 @@ export default class Checkout extends Component {
     const page = this.state.page;
     const card =
       <>
-        <form className='mt-2'>
+        <div className='mt-2'>
           <label className='w-100 text-center'>Nº do cartão </label>
-          <input type="text-area" className="form-control text-center" id='numero_cartao' placeholder="0000-0000-0000-0000" onChange={this.onChange} value={this.state.numero_cartao} />
+          <input className="form-control text-center" id='numero_cartao' placeholder="0000-0000-0000-0000" onChange={this.onChange} value={this.state.numero_cartao} />
 
           <label className='w-100 text-center'>Nome no cartão</label>
-          <input type="text-area" className='form-control text-center' id='nome_titular' placeholder="NOME ESCRITO NO CARTÃO" onChange={this.onChange} value={this.state.nome_titular} />
+          <input className='form-control text-center' id='nome_titular' placeholder="NOME ESCRITO NO CARTÃO" onChange={this.onChange} value={this.state.nome_titular} />
 
-          <label className='w-100 text-center'>Validade</label> <input type="text-area" className='form-control text-center' id='validade_cartao' placeholder="mês/ano" onChange={this.onChange} value={this.state.validade_cartao} />
+          <label className='w-100 text-center'>Validade</label>
+          <input className='form-control text-center' id='validade_cartao' placeholder="mês/ano" onChange={this.onChange} value={this.state.validade_cartao} />
 
           <label className='w-100 text-center'>CVV</label>
-          <input type="text-area"
-            id="cvv" className='form-control text-center'
-            onChange={this.onChange} maxLength='3' placeholder="000" />
+          <input id="cvv" className='form-control text-center' onChange={this.onChange} maxLength='3' placeholder="000" />
 
           <label className='w-100 text-center'>Quantidade de Parcelas</label>
           <select className="custom-select form-control" id="inputGroupSelect02">
@@ -158,16 +312,12 @@ export default class Checkout extends Component {
             <img className="img " src="/img/master.png " width="40px " height="40px " />
             <img className="img " src="/img/boleto.png " width="40px " height="40px " />
           </div>
-        </form>
+        </div>
       </>
 
     const billet =
       <>
         <div className='w-100 h-auto'>
-          {/* <label className='w-100 text-center' htmlFor="boleto_nome">Nome:</label>
-          <input type="text" id='boleto_nome' className='form-control text-center' onChange={this.onChange} />
-          <label className='w-100 text-center' htmlFor="boleto_cpf">CPF:</label>
-          <input type="text" id='boleto_cpf' className='form-control text-center' onChange={this.onChange} /> */}
         </div>
       </>
 
@@ -182,33 +332,15 @@ export default class Checkout extends Component {
 
   render() {
     return (
-
       <>
-
         <div className="row my-5 py-3 center flex-container radius">
 
           <div className="col-12 col-md-4">
             <div className='radius content-enter px-2 py-2 w-100'>
-              <h3 className='w-100 text-center'>Endereço de Entrega</h3>
-              <div className='d-flex justify-content-center align-items-center h-75'>
-                <div className="h-auto">
-                  <label className='w-100 text-center'> Endereço cadastrado: </label>
-                  <select className="custom-select radius" onClick={this.onChange} id="endereco_entrega" >
-                    {this.state.enderecos.map(address => {
-                      const addressComplete = `${address.cep} - ${address.rua},${address.numero} - ${address.bairro} - ${address.cd_uf}`;
-                      return <option key={address.id} value={address.id}>{addressComplete}</option>
-                    })}
 
-                  </select>
-                  <div className='center'>
+              {this.showAdress()}
 
-                    <a className="btn btn-primary btn-lg active mt-5 radius" role="button" aria-pressed="true" >Entregar em outro Endereço</a>
-                  </div>
-                </div>
-              </div>
             </div>
-
-
           </div>
 
           <div className="col-12 col-md-4 my-2">
@@ -240,12 +372,12 @@ export default class Checkout extends Component {
               <div className='d-flex justify-content-center align-items-center h-75'>
                 <div>
                   <label>Frete: </label>
-                  <input type="text" className='form-control text-center' readOnly value='R$ 35,00'/>
+                  <input type="text" className='form-control text-center' readOnly value='R$ 35,00' />
                   <label>Valor Total:</label>
                   <input type="text-area " className='form-control text-center' readOnly value={'R$ ' + this.state.price} />
                   <div className="d-flex justify-content-center mt-5">
-                    <a href="../html/index.html" className="col btn btcc radius">Continuar</a>
-                    <a className="col btn btn-success align-items-center radius" onClick={this.postCards}><label>Comprar</label></a>
+                    <a href="../html/index.html" className="col btn btcc radius mx-1">Continuar</a>
+                    <a className="col btn btn-success align-items-center radius mx-1" onClick={this.postCards}><label>Comprar</label></a>
                   </div>
                 </div>
               </div>
@@ -257,5 +389,4 @@ export default class Checkout extends Component {
 
     );
   }
-
 }
