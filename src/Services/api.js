b90dadf24ed7eea8f1eca1105c50cf699c2765de
Matheus Costa de Oliveira <mcdoliveira@RDL752475.raiadrogasil.com>
2020-11-13 15:58:08 -0300

    att

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
   // baseURL: 'http://desvinho.herokuapp.com/public/api'
-  baseURL: 'http://localhost:8000/api'
+  baseURL: 'http://backend.test/api'
 });
