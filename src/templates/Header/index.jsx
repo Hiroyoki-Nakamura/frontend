4d2d77ed08b0303a85e0c29f7989b9b0fd4387c8
Matheus Costa de Oliveira <mcdoliveira@RDL752475.raiadrogasil.com>
2020-11-20 11:43:34 -0300

    header ajustado com o user name

--- src/templates/Header/index.jsx
+++ src/templates/Header/index.jsx
@@ -22,13 +22,16 @@ export default class Header extends Component {
       <header className="Header central">
         <div className="center row logo">
           <a href="#/"> <div id='logolink' className="center logotipo col-12 col-sm-6 col-md-6 col-lg-6" ></div> </a>
-          <div className="icons acoes center pull-right col-12 col-sm-6 col-md-6 col-lg-6">
+          <div className="icons acoes center col-12 col-sm-6 col-md-6 col-lg-6">
               <If test={this.state.client === ''}>
-                <a href="#/login"><div className="logoUser btn btn_login"></div></a>
-                <div className='text_user'>Login</div>
+                <a className='logoUser' href="#/login"><div className="btn btn_login"></div></a>
+                <a href="#/pedido">
+                  <div className="center">{this.state.client.nome}</div>
+                  <div className="btn btn_login"></div>
+                </a>
               </If>
               <If test={!(this.state.client === '')}>        
-                <a href="#/pedido">{this.state.client.nome}<div className="logoUser btn btn_login"></div></a>
+                <a className='center' href="#/pedido"><div className="logoUser btn btn_login"></div><div className='testando'>{this.state.client.nome}</div></a>
               </If>
               <a href="#/carrinho"><div className="logoCar btn btn_cart"></div></a>
           </div>
